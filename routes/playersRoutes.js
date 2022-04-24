f67d5bc55e818d63f7ca614549482309e0448158
nevisende <ekonomitakipmail@gmail.com>
2022-04-24 18:47:30 +0300

    minor: add the getOnePlayerById to playerRoutes

--- routes/playersRoutes.js
+++ routes/playersRoutes.js
@@ -1,7 +1,16 @@
 const express = require('express')
-const { getPlayersByPageAndSize, createFakePlayers, deleteAllPlayers } = require('../controllers/playersController')
+const {
+  getPlayersByPageAndSize,
+  createFakePlayers,
+  deleteAllPlayers,
+  getOnePlayerById,
+} = require('../controllers/playersController')
 
 const router = express.Router()
+
+router
+  .route('/:playerId')
+  .get(getOnePlayerById)
 
 router
   .route('/')
